728e1241c135017b5590a355b835266e3a293a02
navitha <navithareddy112@gmail.com>
2023-01-06 21:09:32 +0530

    [navitha] UC1CreateContact

---
+++ DAY_10AddressBook/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DAY_10AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1CreateContact addressbook = new UC1CreateContact();
+            addressbook.CreateContact();
+        }
+    }
+}
