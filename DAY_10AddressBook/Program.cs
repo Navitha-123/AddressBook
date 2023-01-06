218786b2f3d77a40a91024f02f9f20c6e1f81d62
navitha <navithareddy112@gmail.com>
2023-01-06 21:16:52 +0530

    [navitha] UC3EditContact

--- DAY_10AddressBook/Program.cs
+++ DAY_10AddressBook/Program.cs
@@ -7,8 +7,14 @@
             //UC1CreateContact addressbook = new UC1CreateContact();
             //addressbook.CreateContact();
 
-            UC2AddContacts add = new UC2AddContacts();
-            add.AddContact();
+            //UC2AddContacts add = new UC2AddContacts();
+            //add.AddContact();
+
+            //UC3EditContact edit = new UC3EditContact();
+            //edit.AddPerson();
+
+            UC4DeleteContact delete = new UC4DeleteContact();
+            delete.DeletePerson();
         }
     }
 }
